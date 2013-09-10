c3528ef2c7c621890bc51e0e84f7dc9431e4ddd4
David Monllao <davidm@moodle.com>
2013-09-10 11:33:35 +0800

    Adapting load_properties to multiple files

--- lib/lib.sh
+++ lib/lib.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Loads configuration and static vars. Should be a first include before moving to other directories.
+# For non-config files the caller script should check that the file exists to provide a more acurate error message.
 load_properties()
 {
     # User configured properties.
-    configfile="./config.properties"
+    configfile="./$1"
     if [ ! -r $configfile ]; then
-        echo "Error: Config file does not exist, copy config.properties.dist and edit the values according to your system"
+        echo "Error: Properties file does not exist, copy $1.dist to $1 and edit the values according to your system"
         exit 1
     fi
     . $configfile
