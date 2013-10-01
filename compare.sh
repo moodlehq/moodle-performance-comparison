a0ee565cd4978fb24fdd5534d40319e1ddb3588b
David Monllao <davidm@moodle.com>
2013-10-01 18:55:15 +0800

    Make compare.sh as simple as possible
    Removing the option to specify a size as
    using S size and 5 loops is enough to have
    consistent results, so everybody can just
    ./compare.sh and wait for a while to, at
    least, have an overall idea of what is
    going on

--- compare.sh
+++ compare.sh
@@ -7,10 +7,7 @@
 # and you can only rely on results like db queries, memory or files included,
 # as this is no controlling the server load at all.
 #
-# Usage: cd /path/to/moodle-performance-comparison && ./compare.sh {size}
-#
-# Arguments:
-# * $1 => Size, one of the following options: XS, S, M, L, XL, XXL. More than 'M' is not recommended for development computers.
+# Usage: cd /path/to/moodle-performance-comparison && ./compare.sh
 #
 ##############################################
 
@@ -24,25 +21,16 @@ load_properties "webserver_config.properties"
 
 timestart=`date +%s`
 
-# We need the size.
-if [ -z "$1" ]; then
-    output="Usage: `basename $0` {size}
-
-Sets up the sites and performs both test runs.
-
-Arguments:
-* $1 => Size, one of the following options: XS, S, M, L, XL, XXL. More than 'M' is not recommended in development computers.
-"
-    echo "$output"
-    exit 1
-fi
-
 # Runs descriptions according to branches.
 # Group name according to date.
 groupname="compare_"`date '+%Y%m%d%H%M'`
 
-./before_run_setup.sh $1
+# Hardcoding S as the size, with 5 loops is enough to have consistent results.
+./before_run_setup.sh S
 ./test_runner.sh "$groupname" "before" -l 5
+# We don't restart the browser here, this is a development machine
+# and probably you are not staring at the CLI waiting for it to
+# finish.
 ./after_run_setup.sh
 ./test_runner.sh "$groupname" "after" -l 5
 
