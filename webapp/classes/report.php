moodlehq/moodle-performance-comparison

commit 81110cbe02cb1eacd9b86e273fa7949aa13b61e7

#6

webapp/classes/report.php
@@ -31,6 +31,11 @@ class report {
     protected $chartsdeclaration = array();
 
     /**
+     * @var array Errors found when comparing runs data.
+     */
+    protected $errors = array();
+
+    /**
      * Loads the test plans.
      *
      * @return void
@@ -94,6 +99,11 @@ class report {
             $run = new test_plan_run($timestamp);
             $run->parse_results();
             $this->runs[] = $run;
+        }
+
+        // Stop when runs are not comparables between them.
+        if (!$this->check_runs_are_comparable()) {
+            return false;
         }
 
         // Will be used to get runs generic data like the steps names, they are supposed to be
@@ -243,6 +253,44 @@ class report {
      */
     public function get_containers() {
         return $this->containers;
+    }
+
+    /**
+     * Returns wheter if errors have been found.
+     *
+     * @return array
+     */
+    public function get_errors() {
+        return $this->errors;
+    }
+
+    /**
+     * Returns true if the runs are comparable between them.
+     *
+     * @return bool True if they are comparable
+     */
+    protected function check_runs_are_comparable() {
+
+        $values = array();
+        foreach (test_plan_run::$runcomparablevars as $var) {
+            foreach ($this->runs as $run) {
+
+                $runvalue = $run->get_run_info()->$var;
+
+                // All should match the fist one.
+                if (empty($values[$var])) {
+                    $values[$var] = $runvalue;
+                }
+
+                if ($values[$var] != $runvalue) {
+                    $this->errors[$var] = "You can not compare runs with a different $var value";
+                }
+            }
+        }
+
+        if (!empty($this->errors)) {
+            return false;
+        }
     }
 
     /**

webapp/classes/report_renderer.php
@@ -179,6 +179,16 @@ class report_renderer {
 
         $output = '';
 
+        // Stop on errors.
+        if ($errors = $this->report->get_errors()) {
+            $output .= '<div class="errors">';
+            foreach ($errors as $error) {
+                $output .= '<div>' . $error . '</div>' . PHP_EOL;
+            }
+            $output .= '</div>';
+            return $output;
+        }
+
         // Number of columns per row.
         $containers = $this->report->get_containers();
         foreach ($this->report->get_charts_declaration() as $chartsdeclaration) {

webapp/classes/test_plan_runs.php
@@ -32,6 +32,11 @@ class test_plan_run {
     );
 
     /**
+     * @var array Vars that needs to be equals across runs to be comparable.
+     */
+    public static $runcomparablevars = array('users', 'rampup', 'size', 'loopcount', 'throughput', 'baseversion');
+
+    /**
      * @var stdClass Run data, including the threads results.
      */
     protected $rundata;

webapp/styles.css
@@ -16,6 +16,14 @@ h2 {
     margin-top: 30px;
 }
 
+.errors {
+    border-style: solid;
+    margin: 50px auto;
+    text-align: center;
+    font-size: 20px;
+    color: red;
+}
+
 .switchtool {
     margin-top: 40px;
     margin-bottom: 50px;

commit d0f99860cc39050cc8460eea44567cdc359e88a1

Fix regression introduced by 81110cbe02cb1eacd9b86e273fa7949aa13b61e7

webapp/classes/report.php
@@ -291,6 +291,8 @@ class report {
         if (!empty($this->errors)) {
             return false;
         }
+
+        return true;
     }
 
     /**