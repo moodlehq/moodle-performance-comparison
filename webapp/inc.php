cfa9865d8b05a800d0dce6ca613eb6a97418e7de
David Monllao <davidm@moodle.com>
2014-02-13 15:06:52 +0800

    Issue #10 - New setting to set readonly web mode

--- webapp/inc.php
+++ webapp/inc.php
@@ -3,6 +3,7 @@
 ini_set('display_errors', true);
 ini_set('error_reporting', E_ALL);
 
+require_once(__DIR__ . '/classes/properties_reader.php');
 require_once(__DIR__ . '/classes/google_charts_renderer.php');
 require_once(__DIR__ . '/classes/google_chart.php');
 require_once(__DIR__ . '/classes/test_plan_runs.php');
